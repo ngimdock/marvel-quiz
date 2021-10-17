33877c35a5d0b93570209a3573398903a96bf1ac
ngimdock <ngimdockzemfack@gmail.com>
2021-10-17 10:26:58 +0100

    gestion des variables d'environnements

--- src/components/Firebase/firebaseConfig.js
+++ src/components/Firebase/firebaseConfig.js
@@ -5,19 +5,12 @@ import "firebase/firestore";
 
 
 const firebaseConfig = {
-	// apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
- //  	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
- //  	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
- //  	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
- //  	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
- //  	appId: process.env.REACT_APP_FIREBASE_APP_ID
-
-  apiKey: "AIzaSyBY9EHsPHZyX2SgVnJngEW5no_qgYW3FQg",
-  authDomain: "marvel-quiz-47dae.firebaseapp.com",
-  projectId: "marvel-quiz-47dae",
-  storageBucket: "marvel-quiz-47dae.appspot.com",
-  messagingSenderId: "333633875562",
-  appId: "1:333633875562:web:1303de4b62a8945dbba413"
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  	appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 class Firebase {
